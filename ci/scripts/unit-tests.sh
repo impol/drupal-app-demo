04eb18610ee8836b54e3cdc0df26ef912a4f86ed
Ubuntu <ubuntu@ip-10-0-1-220.us-west-2.compute.internal>
2017-01-27 19:53:55 +0000

    Add reports

--- ci/scripts/unit-tests.sh
+++ ci/scripts/unit-tests.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
 echo "Executing unit tests..."
+
+mkdir unit-test-reports
+echo "file1" > unit-test-reports/file1
+echo "file2" > unit-test-reports/file2
+echo "file3" > unit-test-reports/file3
+echo "file4" > unit-test-reports/file4
+
+ls unit-test-reports/*
