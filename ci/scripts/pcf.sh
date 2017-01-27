b179e5e7cc36d8ac565d7e6d6a95cf5ef1ffdea7
Ubuntu <ubuntu@ip-10-0-1-220.us-west-2.compute.internal>
2017-01-27 21:35:18 +0000

    Implement CF deployment

--- ci/scripts/pcf.sh
+++ ci/scripts/pcf.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-echo "Pushing application to PCF on branch ${branch}!"
+export APP_NAME="drupal-app-demo-${branch}"
+export DB_NAME="drupaldb-${branch}"
+
+echo "Pushing application ${APP_NAME} to PCF on branch ${branch}..."
+cf push "${APP_NAME}" -b php_buildpack --no-start
+
+echo "Creating ${DB_NAME} database"
+cf create-service p-mysql 100mb "${DB_NAME}"
+
+echo "Binding ${APP_NAME} to ${DB_NAME}..."
+cf bind-service "${APP_NAME}" "${DB_NAME}"
+
+"Starting ${APP_NAME}..."
+cf start "${APP_NAME}"
